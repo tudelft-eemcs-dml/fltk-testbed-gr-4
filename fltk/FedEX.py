f40aaaf814d0c480cdecb261d04bade79956eb74
Yujin-Zhu <yujin.zhu3@gmail.com>
2021-05-29 16:01:33 +0200

    Add initialization of hyperparamters

--- fltk/FedEX.py
+++ fltk/FedEX.py
@@ -1,3 +1,11 @@
+import numpy as np
+
+''' Initialization'''
+k = 5   # Number of possible configurations (batch sizes)
+configurations = [8, 16, 32, 64, 128]
+theta = [1/k]*5   # Probability for each configuration
+
+
 def FedEX():
-    new_batch_size = 1024
+    new_batch_size = 256
     return new_batch_size
