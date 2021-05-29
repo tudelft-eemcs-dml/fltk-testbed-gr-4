27dbbadc49994ac7f74e7c017164c723250ce16f
Yujin-Zhu <yujin.zhu3@gmail.com>
2021-05-29 21:07:34 +0200

    Initialization

--- fltk/FedEX.py
+++ fltk/FedEX.py
@@ -5,7 +5,6 @@ k = 5   # Number of possible configurations (batch sizes)
 configurations = [8, 16, 32, 64, 128]
 theta = [1/k]*5   # Probability for each configuration
 
-
 def FedEX():
     new_batch_size = 256
     return new_batch_size
